893fbbb05065441c24fda73246ed6eeeca8e6396
Mary <kass.o@yandex.ru>
2024-03-17 09:43:02 +0300

    displaying a page with logs, correcting data results, setting up a menu

--- reporting/urls.py
+++ reporting/urls.py
@@ -5,9 +5,5 @@ from reporting.views import ReportingListView
 app_name = 'reporting'
 
 urlpatterns = [
-    # path('create/', MailingsCreateView.as_view(), name='create_mailings'),
-    path('list/', ReportingListView.as_view(), name='reporting_list'),
-    # path('delete/<int:pk>/', MailingsDeleteView.as_view(), name='delete_mailings'),
-    # path('edit/<int:pk>/', MailingsUpdateView.as_view(), name='mailings_update'),
-    # path('view/<int:pk>/', MailingsDetailView.as_view(), name='view_mailings'),
+    path('list/', ReportingListView.as_view(), name='reporting_list')
 ]
--- reporting/views.py
+++ reporting/views.py
@@ -5,3 +5,9 @@ from reporting.models import Reporting
 
 class ReportingListView(ListView):      # страница со всеми логами
     model = Reporting
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        for obj in context['object_list']:
+            obj.status_display = 'Успешно' if obj.status else 'Неуспешно'
+        return context
